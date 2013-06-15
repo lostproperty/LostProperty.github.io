3382a49e6d7abe18ccac36b96d0b6a3626a7d3ee
Rachid Belaid <rachid.belaid@gmail.com>
2013-06-15 21:59:02 +0100

    change heart beat and fix alignment, font

--- lostproperty/build/static/js/main.js
+++ lostproperty/build/static/js/main.js
@@ -1,23 +1,14 @@
-function beatHeart(state){
-    if(state == 1){
-        $('#heart_1').addClass('hide');
-        $('#heart_2').removeClass('hide');
-        state = 2;
-    }else{
-        $('#heart_2').addClass('hide');
-        $('#heart_1').removeClass('hide');
-        state = 1;
-    }
-    return state;
+function beatHeart(){
+    $('#heart').toggleClass('beat');
 }
 
-state = 1;
+
 var authTimer = setInterval(function() {
-    state = beatHeart(state);
-}, 1500);
+    beatHeart();
+}, 2000);
 
 $('.heart-container').click(function() {
-    state = beatHeart(state);
+    beatHeart();
 });
 
 // on roll-over change 0's to 1's

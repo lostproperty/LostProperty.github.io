4d98f7903fbc1c9adcd093ee74e6bd65673cea34
Pete Graham <petegraham1@gmail.com>
2013-06-15 17:36:48 +0100

    adding 404.html and fixing pep8

--- lostproperty/application.py
+++ lostproperty/application.py
@@ -5,31 +5,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return render_template('index.html', home=True)
+    return render_template('index.html', home=True)
+
 
 #@app.route('/services.html')
 def services():
-        return render_template('services.html')
+    return render_template('services.html')
+
 
 @app.route('/robots.txt')
 def robots():
-        return render_template('robots.txt')
+    return render_template('robots.txt')
+
+
+@app.route('/404.html')
+def four_zero_four(name=None):
+    return render_template('404.html')
+
 
 #@app.route('/team.html')
 def team():
-        return render_template('team.html')
+    return render_template('team.html')
+
 
 #@app.route('/contact.html')
 def contact():
-        return render_template('contact.html')
+    return render_template('contact.html')
+
 
 #@app.route('/technology.html')
 def technology():
-        return render_template('technology.html')
+    return render_template('technology.html')
 
-#@app.route('/work.html')
+
 def work(name=None):
-        return render_template('work.html')
+    return render_template('work.html')
+
 
 if __name__ == '__main__':
     app.debug = True

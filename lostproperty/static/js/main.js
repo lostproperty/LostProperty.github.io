33417f241c6629a9e79172909abc6a1d72a54e98
Pete Graham <petegraham1@gmail.com>
2013-06-15 13:49:12 +0100

    Beating heart in.

--- lostproperty/static/js/main.js
+++ lostproperty/static/js/main.js
@@ -1,1 +1,24 @@
+function beatHeart(state){
+    if(state == 1){
+        $('#heart_1').addClass('hide');
+        $('#heart_2').removeClass('hide');
+        state = 2;
+    }else{
+        $('#heart_2').addClass('hide');
+        $('#heart_1').removeClass('hide');
+        state = 1;
+    }
+    return state;
+}
 
+state = 1;
+var authTimer = setInterval(function() {
+    state = beatHeart(state);
+}, 1500);
+
+$('.heart-container').click(function() {
+    state = beatHeart(state);
+});
+
+// on roll-over change 0's to 1's
+// can we make this "wave" through the ascii?
